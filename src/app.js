1c5a598e362ea12abfa199d2a36674804f94ed63
Vasudev Yadav <vasuxcode@gmail.com>
2026-07-27 14:59:57 +0530

    27 july app.js

--- src/app.js
+++ src/app.js
@@ -8,8 +8,8 @@ import apiRouter from "./routes/index.js";
 const app = express();
 
 app.use(cors());
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json({ limit: "10mb" }));
+app.use(express.urlencoded({ extended: true, limit: "10mb" }));
 app.use(apiRateLimiter);
 
 app.use("/api", apiRouter);
